c5ca1f1d25617260f84b98d639d70c88ad8c36e5
yukiyan <wakayama0215@gmail.com>
2016-10-12 03:01:52 +0900

    :recycle: Extract function

--- functions/notify-auto-scaling-event/index.js
+++ functions/notify-auto-scaling-event/index.js
@@ -4,51 +4,46 @@ const IncomingWebhooks = require('@slack/client').IncomingWebhook;
 const slack = new IncomingWebhooks(process.env.SLACK_WEBHOOK_URL);
 const slackChannel = process.env.CHANNEL;
 
+const sendSlack = payload => new Promise((resolve, _reject) => {
+  resolve(slack.send(payload));
+});
+
 exports.handle = (event, context, callback) => {
   const message = JSON.parse(event.Records[0].Sns.Message);
-  const eventName = message.Event;
-  console.log('message ->', message);
-  console.log('eventName ->', eventName);
-
-  if (eventName === 'autoscaling:EC2_INSTANCE_LAUNCH' || eventName === 'autoscaling:EC2_INSTANCE_TERMINATE') {
-    const sendSlack = () => {
-      const slackMessage = {
-        username: 'オートスケールイベント知らせるくん',
-        channel: slackChannel,
-        iconEmoji: ':speaking_head_in_silhouette:',
-        attachments: [
+  const slackMessage = {
+    username: 'オートスケールイベント知らせるくん',
+    channel: slackChannel,
+    iconEmoji: ':speaking_head_in_silhouette:',
+    attachments: [
+      {
+        fallback: 'オートスケールイベントが発生しました',
+        color: '#36a64f',
+        author_name: 'notify-auto-scaling-event',
+        title: 'オートスケールイベントが発生しました :rotating_light:',
+        fields: [
           {
-            fallback: 'オートスケールイベントが発生しました',
-            color: '#36a64f',
-            author_name: 'notify-auto-scaling-event',
-            title: 'オートスケールイベントが発生しました :rotating_light:',
-            fields: [
-              {
-                title: 'Auto scaling group',
-                value: message.AutoScalingGroupName,
-                short: false,
-              },
-              {
-                title: 'Event name',
-                value: eventName.match(/autoscaling:(\w+)/)[1],
-                short: false,
-              },
-              {
-                title: 'Message',
-                value: message.Cause,
-                short: false,
-              },
-            ],
-            footer: 'EC2 Auto scale → SNS → lambda → slack',
+            title: 'Auto scaling group',
+            value: message.AutoScalingGroupName,
+            short: false,
+          },
+          {
+            title: 'Event name',
+            value: message.Event.match(/autoscaling:(\w+)/)[1],
+            short: false,
+          },
+          {
+            title: 'Message',
+            value: message.Cause,
+            short: false,
           },
         ],
-      };
-      return new Promise((resolve, _reject) => {
-        resolve(slack.send(slackMessage));
-      });
-    };
+        footer: 'EC2 Auto scale → SNS → lambda → slack',
+      },
+    ],
+  };
 
-    sendSlack()
+  if (message.Event === 'autoscaling:EC2_INSTANCE_LAUNCH' || message.Event === 'autoscaling:EC2_INSTANCE_TERMINATE') {
+    sendSlack(slackMessage)
       .then((response) => {
         console.log('response ->', response);
         callback(null, 'success');
